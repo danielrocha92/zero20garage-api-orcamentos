70bd840bb36418ad0b144e6b08124e2c0bec165a
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-08-05 16:47:43 -0300

    ok

--- src/config/db.js
+++ src/config/db.js
@@ -4,16 +4,18 @@ const path = require('path');
 // Caminho para o arquivo JSON
 const serviceAccount = require(path.join(__dirname, 'firebase-service-account.json'));
 
-try {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-  console.log('Firebase Admin SDK inicializado com sucesso.');
-} catch (error) {
-  console.error('Erro ao inicializar Firebase Admin SDK:', error.message);
-  if (!/already been called/.test(error.message)) {
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    console.log('Firebase Admin SDK inicializado com sucesso.');
+  } catch (error) {
+    console.error('Erro ao inicializar Firebase Admin SDK:', error.message);
     process.exit(1);
   }
+} else {
+  console.log('Firebase Admin SDK já estava inicializado.');
 }
 
 const db = admin.firestore();
